98e8f72794460e670ac5e4e962652c489db4145c
bhanushishodia <mobiweb.developer2@gmail.com>
2026-05-03 09:41:59 +0530

    remove export mode fix public files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",   // 👈 yeh line add karo
+  // output: "export",  
   images: {
     unoptimized: true,
   },
